2fd696c6504c6c2fa479cd15f7ac082376938098
Ormeon12 <guingantevan@gmail.com>
2025-02-07 18:35:07 +0100

    Add files via upload

---
+++ Web_Marketing_Evan_Guingant_Grp4/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".btn.black").addEventListener("click", function () {
+        alert("Essayez nos plats végétariens !");
+    });
+});
+
+const reload = "main is loaded!"
+console.log(reload);
+
+
+
+
+
